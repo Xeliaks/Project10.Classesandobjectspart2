89523e3a1f586da28a37de67c40209d601ca7a9c
Xeliaks <borachukbogdan@gmail.com>
2023-10-05 16:57:25 +0300

    Something

--- src/Part2CollectionOfHobbyObjects/Books.java
+++ src/Part2CollectionOfHobbyObjects/Books.java
@@ -2,58 +2,61 @@ package Part2CollectionOfHobbyObjects;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Books {
-
     private int count;
 
 
-
     private ArrayList<Book> books;
-    public Books(){
+
+    public Books() {
         books = new ArrayList<>();
     }
 
-    public void addBook(Book b){
+    public void addBook(Book b) {
         books.add(b);
     }
 
-    public void printOne(int i){
-        System.out.println("Book you required: " + books.get(i));
+    public void printOne(int i) {
+        if (i >= 0 && i < books.size()) {
+            System.out.println("Book you required: " + " Title: " + books.get(i).title + " Author: " + books.get(i).author +
+                    " Price: " + books.get(i).getPrice() + " Year: " + books.get(i).getYear());
+        } else {
+            System.out.println("Book not found");
+        }
     }
 
-    public void print(){
+    public void print() {
         count = 1;
         System.out.println("Full collection of books: ");
-        for (int i=0;i<books.size();i++){
-            System.out.println("Book " + count + " :" + books.get(i));
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println("Book " + count + " :" + " Title: " + books.get(i).title + " Author: " + books.get(i).author +
+                    " Price: " + books.get(i).getPrice() + " Year: " + books.get(i).getYear());
             count++;
         }
     }
 
-    public Book find(String s){
-        for (int i=0;i< books.size();i++){
-            if (books.get(i).title.equalsIgnoreCase(s)){
-                System.out.println("Your book");
-                return books.get(i);
+    public int find(String s) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).title.equalsIgnoreCase(s)) {
+                System.out.println("Your book: ");
+                return i;
             }
         }
-        System.out.println("Book not found");
-        return null;
+
+        return -1;
     }
 
-    public void sort(){
-        int temp;
+    public void sort() {
         boolean swapped;
         for (int i = 0; i < books.size() - 1; i++) {
             swapped = false;
             for (int j = 0; j < books.size() - i - 1; j++) {
-                if (books.get(j).getYear() > books.get(j+1).getYear()) {
+                if (books.get(j).getYear() > books.get(j + 1).getYear()) {
 
+                    Collections.swap(books, j, j + 1);
 
-                    temp = books.get(j).getYear();
-                    books.get(j+1).setYear(books.get(j).getYear());
-                    books.get(j).setYear(temp);
                     swapped = true;
                 }
             }
@@ -64,17 +67,14 @@ public class Books {
 
     }
 
-    public Book findYear(int s){
-        for (int i=0;i< books.size();i++){
-            if (books.get(i).getYear() == s){
+    public int findYear(int s) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getYear() == s) {
                 System.out.println("Your book");
-                return books.get(i);
+                return i;
             }
         }
-        System.out.println("Book not found");
-        return null;
+
+        return -1;
     }
-
-
-
 }
--- src/Part2CollectionOfHobbyObjects/Main.java
+++ src/Part2CollectionOfHobbyObjects/Main.java
@@ -2,6 +2,41 @@ package Part2CollectionOfHobbyObjects;
 import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
 
 
+        Book book1 = new Book("Red Prince", "John Grey");
+        Book book2 = new Book("Winter Witch", "Eugene White");
+        Book book3 = new Book("Black Knight", "Jack Black");
+        Book book4 = new Book("Purple Scarf", "Yuri Wong");
+        Book book5 = new Book("Orange sands", "Alex Green");
+
+        book1.setPrice(12.55);
+        book2.setPrice(13.49);
+        book3.setPrice(9.99);
+        book4.setPrice(18.29);
+        book5.setPrice(7.99);
+
+        book1.setYear(2012);
+        book2.setYear(2009);
+        book3.setYear(2013);
+        book4.setYear(2014);
+        book5.setYear(2010);
+
+        Books books = new Books();
+        books.addBook(book1);
+        books.addBook(book2);
+        books.addBook(book3);
+        books.addBook(book4);
+        books.addBook(book5);
+
+        books.print();
+        System.out.println();
+        books.printOne(books.find("Strange"));
+        System.out.println();
+        books.printOne(books.find("Red Prince"));
+        System.out.println();
+        books.sort();
+        books.print();
+    }
 }
